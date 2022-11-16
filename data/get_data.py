46561a741490f33124141830af857d97a44c529e
jamieyu0914 <jamieyu0914@gmail.com>
2022-11-17 00:05:03 +0800

    3st develop Commit 1116 -- feat complete the file_link process and done the get data to database

--- data/get_data.py
+++ data/get_data.py
@@ -42,40 +42,57 @@ for i in range(0,nums):
     file = str(attractions["file"]) #從檔案連結中
     #print(file)
 
-#     #資料庫上傳景點資料
-#     sql = "INSERT INTO attractions_list (id, name, category, description, address, transport, mrt, lat, lng) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-#     val = (id, name, category, description, address, transport, mrt, lat, lng)
-#     try:
-#         # Get connection object from a pool
-#         connection_object = connection_pool.get_connection() #連線物件 commit時 需要使用
-#         cursor = connection_object.cursor()
-#         #print("MySQL connection is opened")
-#         cursor.execute(sql, val)
-#         connection_object.commit()
-#     except Error as e:
-#         print("Error while connecting to MySQL using Connection pool ", e)
-#     finally:
-#         # closing database connection.    
-#         cursor.close()
-#         connection_object.close()
-#     #print("MySQL connection is closed")
-# print("DONE!")                 
+    #資料庫上傳景點資料
+    sql = "INSERT INTO attractions_list (id, name, category, description, address, transport, mrt, lat, lng) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    val = (id, name, category, description, address, transport, mrt, lat, lng)
+    try:
+        # Get connection object from a pool
+        connection_object = connection_pool.get_connection() #連線物件 commit時 需要使用
+        cursor = connection_object.cursor()
+        #print("MySQL connection is opened")
+        cursor.execute(sql, val)
+        connection_object.commit()
+    except Error as e:
+        print("Error while connecting to MySQL using Connection pool ", e)
+    finally:
+        # closing database connection.    
+        cursor.close()
+        connection_object.close()
+    #print("MySQL connection is closed")
+print("DONE!")                 
 
 
-    #處理圖片資料
-    for j in range(0,1):
-        print(id)
-        nums = (len(attractions["file"].split("https"))) #取得圖片數量
-        #print(nums)
-        for k in range(1,nums):
-            a = attractions["file"].split("https")[k] #分隔後將第k張圖片連結存入 a 陣列
-            file_link = "https"+a
-            number = str(k)
-            if (file_link.find('.jpg')>0 or file_link.find('.JPG')>0 or file_link.find('.png')>0 or file_link.find('.PNG')>0): #過濾資料中，不是 JPG 或 PNG 的檔案
-                print(number+'--'+file_link)
+#處理圖片資料
+for j in range(0,1):
+    print(id)
+    nums = (len(attractions["file"].split("https"))) #取得圖片數量
+    #print(nums)
+    for k in range(1,nums):
+        a = attractions["file"].split("https")[k] #分隔後將第k張圖片連結存入 a 陣列
+        file_link = "https"+a
+        number = str(k)
+        if (file_link.find('.jpg')>0 or file_link.find('.JPG')>0 or file_link.find('.png')>0 or file_link.find('.PNG')>0): #過濾資料中，不是 JPG 或 PNG 的檔案
+                #資料庫上傳景點圖片
+            sql = "INSERT INTO images_list (attractions_id, link) VALUES (%s, %s)"
+            val = (id, file_link)
+            try:
+                # Get connection object from a pool
+                connection_object = connection_pool.get_connection() #連線物件 commit時 需要使用
+                cursor = connection_object.cursor()
+                #print("MySQL connection is opened")
+                cursor.execute(sql, val)
+                connection_object.commit()
+            except Error as e:
+                print("Error while connecting to MySQL using Connection pool ", e)
+            finally:
+                # closing database connection.    
+                cursor.close()
+                connection_object.close()
+            #print("MySQL connection is closed")
+        print("DONE!")         
 
 
-
+   
     
             
       
