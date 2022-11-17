836eccc250b9ff65b3e1827b2bf1ce7488bf712d
jamieyu0914 <jamieyu0914@gmail.com>
2022-11-18 04:02:50 +0800

    1st develop Commit 1118 -- feat /api/attraction/{attractionId}

--- app.py
+++ app.py
@@ -23,7 +23,9 @@ connection_pool = pooling.MySQLConnectionPool(pool_name="my_connection_pool",
 app=Flask(__name__,
     static_folder="public",
     static_url_path="/") #建立 Application 物件)
+app.secret_key="any string but secret" #設定 Session 的密鑰    
 app.config["JSON_AS_ASCII"]=False
+app.config["JSON_SORT_KEYS"]=False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
 
 # Pages
@@ -40,20 +42,119 @@ def booking():
 def thankyou():
 	return render_template("thankyou.html")
 @app.route("/api/attraction", methods=["GET"])
-def api_attraction(): #要取得的分頁，每頁 12 筆資料
-    page = request.args.get("page","")
-    nextPage = int(page)+1
-    input = request.args.get("input","")
-    print(input)
-    if(page == ''):
+def api_attraction(): 
+    page = request.args.get("page","")#分頁
+    print(page)
+    if(page == ""):
         page = 0 #minimum: 0  
-    if(input == ""):
-        input == "*"
-    else:
-        sql = "SELECT attractions_list.id, attractions_list.name, attractions_list.category, attractions_list.description, attractions_list.address, attractions_list.transport, attractions_list.mrt, attractions_list.lat, attractions_list.lng, images_list.link FROM attractions_list INNER JOIN images_list ON images_list.attractions_id=attractions_list.id WHERE category=%s;" #SQL指令 是否有對應的帳號、密碼
-        val = (input,)
+    nextPage = int(page)+1
+
+    keyword = request.args.get("keyword","")
+    print(keyword)
+    if(keyword == ""):
+        keyword == "*"
+
+    sql = "SELECT attractions_list.id, attractions_list.name, attractions_list.category, attractions_list.description, attractions_list.address, attractions_list.transport, attractions_list.mrt, attractions_list.lat, attractions_list.lng, images_list.link FROM attractions_list INNER JOIN images_list ON images_list.attractions_id=attractions_list.id WHERE category=%s;" #SQL指令 是否有對應的帳號、密碼
+    val = (keyword, )
+    try:
+        # Get connection object from a pool
+        connection_object = connection_pool.get_connection() #連線物件 commit時 需要使用
+        cursor = connection_object.cursor()
+        #print("MySQL connection is opened")
+        cursor.execute(sql,val)
+        myresult = cursor.fetchall()
+        x=""
+        for x in myresult:
+            id = x[0]
+            name = x[1]
+            category = x[2]
+            description = x[3]
+            a = x[4].split(" ")[0]+x[4].split(" ")[1]
+            address = a+x[4].split(" ")[1]+x[4].split(" ")[2]
+            transport = x[5]
+            mrt = x[6]
+            lat = x[7]
+            lng = x[8]
+            link = x[9]    
+        rows = cursor.rowcount #得出結果列數
+        print(rows)
+    except Error as e:
+            print("Error while connecting to MySQL using Connection pool ", e)
+            return jsonify({"error":True, "message": "伺服器內部錯誤"}),500
+    finally:
+        # closing database connection.    
+        cursor.close()
+        connection_object.close()
+        #print("MySQL connection is closed")
+        print("DONE!")
+
+    if(x==""):
+        keyword2 = "%"+f"{keyword}"+"%"
+        sql = "SELECT attractions_list.id, attractions_list.name, attractions_list.category, attractions_list.description, attractions_list.address, attractions_list.transport, attractions_list.mrt, attractions_list.lat, attractions_list.lng, images_list.link FROM attractions_list INNER JOIN images_list ON images_list.attractions_id=attractions_list.id WHERE name LIKE %s;" #SQL指令 是否有對應的帳號、密碼
+        val = (keyword2, )
         try:
-			# Get connection object from a pool
+            # Get connection object from a pool
+            connection_object = connection_pool.get_connection() #連線物件 commit時 需要使用
+            cursor = connection_object.cursor()
+            #print("MySQL connection is opened")
+            cursor.execute(sql,val)
+            myresult = cursor.fetchall()
+            x=''
+            for x in myresult:
+                id = x[0]
+                name = x[1]
+                category = x[2]
+                description = x[3]
+                a = x[4].split(" ")[0]+x[4].split(" ")[1]
+                address = a+x[4].split(" ")[1]+x[4].split(" ")[2]
+                transport = x[5]
+                mrt = x[6]
+                lat = x[7]
+                lng = x[8]
+                link = x[9]  
+            rows = cursor.rowcount #得出結果列數
+            print(rows)
+            if(page == int(rows//12)): #得出結果列數
+                nextPage == None
+            return jsonify({
+                    "nextPage":int(nextPage),
+                    "data":[{
+                        "id":int(id),
+                        "name":str(name),
+                        "category":str(category),
+                        "description":str(description),
+                        "address":str(address),
+                        "transport":str(transport),
+                        "mrt":str(mrt),
+                        "lat":float(lat),
+                        "lng":float(lng),
+                        "images":[link]
+                        }]
+                    }     ), 200            
+        except Error as e:
+            print("Error while connecting to MySQL using Connection pool ", e)
+            return jsonify({"error":True, "message": "伺服器內部錯誤"}), 500
+        finally:
+            # closing database connection.    
+            cursor.close()
+            connection_object.close()
+            #print("MySQL connection is closed")
+            print("DONE!")
+
+
+@app.route("/api/attraction/{attractionId}", methods=["GET"])
+def caculate():
+        Number = request.args.get("id","")
+        Number = str(Number)
+        print(Number)
+
+        if(Number==""):
+            Number == "*"
+            
+        sql = "SELECT attractions_list.id, attractions_list.name, attractions_list.category, attractions_list.description, attractions_list.address, attractions_list.transport, attractions_list.mrt, attractions_list.lat, attractions_list.lng, images_list.link FROM attractions_list INNER JOIN images_list ON images_list.attractions_id=attractions_list.id WHERE id=%s;" #SQL指令 是否有對應的帳號、密碼
+        val = (Number, )
+        try:
+            # Get connection object from a pool
             connection_object = connection_pool.get_connection() #連線物件 commit時 需要使用
             cursor = connection_object.cursor()
             #print("MySQL connection is opened")
@@ -72,30 +173,37 @@ def api_attraction(): #要取得的分頁，每頁 12 筆資料
                 lat = x[7]
                 lng = x[8]
                 link = x[9]    
-                #print(x)
+            rows = cursor.rowcount #得出結果列數
+            print(rows)
+            return jsonify({                 
+                    "data":{
+                        "id":int(id),
+                        "name":str(name),
+                        "category":str(category),
+                        "description":str(description),
+                        "address":str(address),
+                        "transport":str(transport),
+                        "mrt":str(mrt),
+                        "lat":float(lat),
+                        "lng":float(lng),
+                        "images":[link]
+                        }
+                    }     ), 200            
         except Error as e:
             print("Error while connecting to MySQL using Connection pool ", e)
+            return jsonify({"error":True, "message": "伺服器內部錯誤"}), 500
         finally:
             # closing database connection.    
             cursor.close()
             connection_object.close()
             #print("MySQL connection is closed")
-            print("DONE!")     
-            return jsonify({
-                "nextPage":int(nextPage),
-            	"data":[{
-            		"id":int(id),
-		            "name":str(name),
-		            "category":str(category),
-                    "description":str(description),
-                    "address":str(address),
-                    "transport":str(transport),
-                    "mrt":str(mrt),
-                    "lat":float(lat),
-                    "lng":float(lng),
-                    "images":[str(link)]
-		            }]
-	            }     )
+            print("DONE!")
 
+
+
+    
+    
+
+            
 
 app.run(port=3000)
