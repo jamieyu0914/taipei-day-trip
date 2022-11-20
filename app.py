08f46cdae531735ee7e902660b9563f39d565acc
jamieyu0914 <jamieyu0914@gmail.com>
2022-11-20 11:38:09 +0800

    4st develop Commit 1119 -- Final Version for ubuntu in AWS -- fix /api/attraction

--- app.py
+++ app.py
@@ -44,18 +44,25 @@ def thankyou():
 @app.route("/api/attraction", methods=["GET"])
 def api_attraction(): 
     page = request.args.get("page","")#分頁
+    keyword = request.args.get("keyword","")
+    print(keyword)
     print(page)
     if(page == ""):
         page = 0 #minimum: 0  
+    
     nextPage = int(page)+1
+    nums = (int(page)*12)
+    print(nums) 
+     
+    keyword2 = "%"+f"{keyword}"+"%"
+    print(keyword2)   
 
-    keyword = request.args.get("keyword","")
-    #print(keyword)
     if(keyword == ""):
         keyword == "*"
+        keyword2 == "*"
 
-    sql = "SELECT attractions_list.id, attractions_list.name, attractions_list.category, attractions_list.description, attractions_list.address, attractions_list.transport, attractions_list.mrt, attractions_list.lat, attractions_list.lng, merge_images_list.links FROM attractions_list INNER JOIN merge_images_list ON merge_images_list.attractions_id=attractions_list.id WHERE category=%s;" #SQL指令 
-    val = (keyword, )
+    sql = "SELECT attractions_list.id, attractions_list.name, attractions_list.category, attractions_list.description, attractions_list.address, attractions_list.transport, attractions_list.mrt, attractions_list.lat, attractions_list.lng, merge_images_list.links FROM attractions_list INNER JOIN merge_images_list ON merge_images_list.attractions_id=attractions_list.id WHERE category= %s OR name LIKE %s LIMIT %s,12;" #SQL指令 
+    val = (str(keyword), str(keyword2), nums)
     try:
         # Get connection object from a pool
         connection_object = connection_pool.get_connection() #連線物件 commit時 需要使用
@@ -90,94 +97,19 @@ def api_attraction():
                     "images":links
                     }
             results.append(result)
-        page = int(page)    
-        start =  (page*12)
-        print(start)
-        end = ((page+1)*12)
-        print(end)    
-        #print(results[start:end])
         rows = cursor.rowcount #得出結果列數
         print('筆數='+str(rows))
-        if (rows<=12):
-            results = results;
-        else:
-            results = results[start:end]                 
+            
     except Error as e:
-            print("Error while connecting to MySQL using Connection pool ", e)
-            return (jsonify({"error":True, "message": "伺服器內部錯誤"})),500
+        #print("Error while connecting to MySQL using Connection pool ", e)
+        return (jsonify({"error":True, "message": "伺服器內部錯誤"})),500
     finally:
         # closing database connection.    
         cursor.close()
         connection_object.close()
-        # print("MySQL connection is closed")
+        #print("MySQL connection is closed")
         print("DONE!")
 
-    if(rows==0):
-        keyword2 = "%"+f"{keyword}"+"%"
-        sql = "SELECT attractions_list.id, attractions_list.name, attractions_list.category, attractions_list.description, attractions_list.address, attractions_list.transport, attractions_list.mrt, attractions_list.lat, attractions_list.lng, merge_images_list.links FROM attractions_list INNER JOIN merge_images_list ON merge_images_list.attractions_id=attractions_list.id WHERE name LIKE %s;" #SQL指令 
-        val = (keyword2, )
-        try:
-            # Get connection object from a pool
-            connection_object = connection_pool.get_connection() #連線物件 commit時 需要使用
-            cursor = connection_object.cursor()
-            #print("MySQL connection is opened")
-            cursor.execute(sql,val)
-            myresult = cursor.fetchall()
-            x=''
-            results = []
-            for x in myresult:
-                id = x[0]
-                name = x[1]
-                category = x[2]
-                description = x[3]
-                a = x[4].split(" ")[0]+x[4].split(" ")[1]
-                address = a+x[4].split(" ")[1]+x[4].split(" ")[2]
-                transport = x[5]
-                mrt = x[6]
-                lat = x[7]
-                lng = x[8]
-                links = x[9].split(',')
-                result = {
-                        "id":int(id),
-                        "name":str(name),
-                        "category":str(category),
-                        "description":str(description),
-                        "address":str(address),
-                        "transport":str(transport),
-                        "mrt":str(mrt),
-                        "lat":float(lat),
-                        "lng":float(lng),
-                        "images":links
-                        }
-                results.append(result)
-            page = int(page)    
-            start =  (page*12)
-            print(start)
-            end = ((page+1)*12)
-            print(end)    
-            #print(results[start:end])
-            rows = cursor.rowcount #得出結果列數
-            print('筆數='+str(rows))
-            if (rows<=12):
-                results = results;
-            else:
-                results = results[start:end]
-            print(int(rows/12))
-            
-        except Error as e:
-            #print("Error while connecting to MySQL using Connection pool ", e)
-            return (jsonify({"error":True, "message": "伺服器內部錯誤"})),500
-        finally:
-            # closing database connection.    
-            cursor.close()
-            connection_object.close()
-            #print("MySQL connection is closed")
-            print("DONE!")
-    nextPage = int(nextPage)                     
-    if(page == int(rows/12)): #得出結果列數
-        nextPage = None        
-    elif(page > int(rows/12)):
-                return (jsonify({"error":True, "message": "已超出結果頁面"})),400        
     return jsonify({
             "nextPage":nextPage,
             "data": results
